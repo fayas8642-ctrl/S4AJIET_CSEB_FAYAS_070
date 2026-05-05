4fd88ee026f34043528fba7c507c6e13319804cb
fayas8642-ctrl <fayas8642@gmail.com>
2026-05-05 13:57:23 +0530

    Create reverse.java

---
+++ reverse.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int reverse(int x) {
+    
+        int l;
+
+	    int rev=0;
+	  
+	   while(x!=0){
+	       l=x%10;
+            if (rev > Integer.MAX_VALUE / 10 || rev < Integer.MIN_VALUE / 10) {
+                return 0;
+            }
+	       rev=rev*10+l;
+	       x=x/10;
+	       
+	   }
+	  return rev;
+    }
+}        
+    
+
