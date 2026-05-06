ec66b5ad90ecbd86a6de0dec57e4326b1493dcb6
fayas8642-ctrl <fayas8642@gmail.com>
2026-05-06 11:42:25 +0530

    Create hashset.java

---
+++ hashset.java
@@ -0,0 +1,28 @@
+import java.util.LinkedList;
+import java.util.HashSet;
+
+public class Main
+{
+	public static void main(String[] args) {
+	  HashSet<Integer>cse=new HashSet<>();
+	  cse.add(10);
+	 cse.add(56);
+	
+	    System.out.println(cse);
+	  cse.add(0);
+	 
+	  System.out.println(cse);
+	  cse.remove(0);
+	   System.out.println(cse);
+	    System.out.println(cse.size());
+	  cse.clear();
+	   System.out.println(cse);
+	  
+	
+	  
+	   
+	    
+	     
+	   
+	}
+}
